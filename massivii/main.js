f4fae210886a76094b31a538c7c8dd30e2e90d44
Gancevkirill <144214859+Gancevkirill@users.noreply.github.com>
2024-01-29 11:32:41 +0300

    Add files via upload

---
+++ massivii/main.js
@@ -0,0 +1,55 @@
+//let arr = ['a', 'b', 'c'];
+//alert(arr);
+
+
+//let arr = ['a', 'b', 'c'];
+//alert(arr[0]);
+//alert(arr[1]);
+//alert(arr[2]);
+
+
+//let arr = ['a', 'b', 'c', 'd'];
+//alert(arr[0] + arr[1] + ', ' + arr[2] + arr[3]);
+
+
+//let arr = [2, 5, 3, 9];
+//let result = arr[0] * arr[1] + arr[2] * arr[3];
+//alert(result);
+
+
+//var obj = {a: 1, b: 2, c: 3};
+//alert(obj['c']);
+//alert(obj.c);
+
+
+//var obj = {Коля: '1000', Вася: '500', Петя: '200'};
+//alert(obj.Коля);
+//alert(obj.Петя);
+
+
+//var obj = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'};
+//alert(obj[3]);
+
+
+//var obj = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'};
+//var day = 3;
+//alert(obj[day]);
+
+
+//let arr = [[1, 2, 3], [4, 5, 6], [7,8,9]];
+//alert(arr[1][0]);
+
+
+//let obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+//alert(obj.js[0]);
+
+
+//let arr = {ru: ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'], en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']};
+//alert(arr.ru[0]);
+//alert(arr.en[2]);
+
+
+//let arr = {ru: ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'], en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']};
+//let lang = 'ru';
+//let day = 3;
+//alert(arr[lang][day - 1]);
